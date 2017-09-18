961c7faa5cddbb2594b264f4389d197af2dc6423
Nochnyi Valerii <Valerii_Nochnyi@epam.com>
2017-09-18 18:21:55 +0300

    commit_1

---
+++ Main.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Valerii_Nochnyi on 2/22/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+//        System.out.println("4/2=" + 4/2 == "4/2=2");
+
+//    String s1 = "hi";
+//    String s2 = "hi";
+//        System.out.println(s1 == s2);
+//        System.out.println(s1.equals(s2));
+
+        byte s1 = 0, s2 = -99;
+        byte  s3 = (byte) (s1 + s2);
+
+        System.out.println(s3);
+
+    }
+}
---
+++ Main2.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Valerii_Nochnyi on 9/18/2017.
+ */
+public class Main2 {
+
+}
